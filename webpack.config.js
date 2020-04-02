bbac088685c1b327837e3156671dc696f986f9c9
Solej77 <ing.josgalo@gmail.com>
2020-04-01 19:17:37 -0600

    Configuration and deploy with now

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   output: {
     filename: 'app.bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
   ]
 }
