3d92b0c8e98b988ad5518c76be1e0495eb5811f4
Solej77 <ing.josgalo@gmail.com>
2020-04-03 12:09:30 -0600

    Use of Intersection Observer polyfill and dinamycs imports

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -10,20 +10,26 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    // element.current, es nuestro elemento del dom, para este componente es el article
-    // console.log(element.current)
-
-    const obersver = new window.IntersectionObserver(function(entries) {
-      const { isIntersecting } = entries[0]
-      console.log(isIntersecting)
-
-      if (isIntersecting) {
-        setShow(true)
-        obersver.disconnect()
-      }
+    // Cargar el Polyfill intersection-observer, en caso de que no los soporte el navegador
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+      ? window.IntersectionObserver
+      : import('intersection-observer')
+    ).then(() =>{
+      // element.current, es nuestro elemento del dom, para este componente es el article
+      // console.log(element.current)
+      const obersver = new window.IntersectionObserver(function(entries) {
+        const { isIntersecting } = entries[0]
+  
+        if (isIntersecting) {
+          setShow(true)
+          obersver.disconnect()
+        }
+      })
+  
+      obersver.observe(element.current)
     })
 
-    obersver.observe(element.current)
   }, [element])
 
   return(
