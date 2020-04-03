848a619bf5c2f8937f7e5c442db44b33b71c8de2
Solej77 <ing.josgalo@gmail.com>
2020-04-03 00:36:03 -0600

    useEffect: cleaning events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { GlobalStyles } from './styles/GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards' 
 
 export const App = () => (
   <>
-    <GlobalStyles />
+    <GlobalStyle />
     <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -5,21 +5,40 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)  
 
-useEffect(() => {
-  fetch('https://petgram-server-solej77.now.sh/categories')
-    .then(res => res.json())
-    .then(data => {
-      setCategories(data)
-    })
-}, [])
-// el array vacio indica que solo se debe de mostar un sola vez cuando es renderizado
+  useEffect(() => {
+    fetch('https://petgram-server-solej77.now.sh/categories')
+      .then(res => res.json())
+      .then(data => {
+        setCategories(data)
+      })
+  }, [])
+  // el array vacio indica que solo se debe de mostar un sola vez cuando es renderizado
 
-  return(
-    <List>
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id} ><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return(
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
