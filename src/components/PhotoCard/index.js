da5bd095ad1e90827b9a275dca57f88766c39de9
Solej77 <ing.josgalo@gmail.com>
2020-04-03 12:55:11 -0600

    Using localStorage for savelikes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -8,7 +8,17 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
   // nos va a permitir capturtar el elemento del DOM
   const element = useRef(null)
   const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key)
+      return like
+    } catch (e) {
+      return false
+    }
+  })
 
+  console.log(liked)
   useEffect(() => {
     // Cargar el Polyfill intersection-observer, en caso de que no los soporte el navegador
     Promise.resolve(
@@ -32,6 +42,16 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
 
   }, [element])
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return(
     <Article ref={element}>
       {
@@ -42,8 +62,8 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size='32px'/> {likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size='32px'/> {likes} likes!
           </Button>
         </>
       }
