d8c2fc68dc061abd65eeb5ee3d8bf7e4c3add4c8
Solej77 <ing.josgalo@gmail.com>
2020-04-03 00:00:40 -0600

    Implementing useState and useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
-import { categories } from '../../../api/db.json'
-
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+useEffect(() => {
+  fetch('https://petgram-server-solej77.now.sh/categories')
+    .then(res => res.json())
+    .then(data => {
+      setCategories(data)
+    })
+}, [])
+// el array vacio indica que solo se debe de mostar un sola vez cuando es renderizado
+
   return(
     <List>
       {
