d416faf7c9887990f52175d9040d3c7a6e004827
Solej77 <ing.josgalo@gmail.com>
2020-04-03 15:51:15 -0600

    Initializing React Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
-import { App } from './App'; 
+import { App } from './App';
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server-solej77.now.sh/graphql',
+})
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client} >
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 )
