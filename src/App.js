a8cd2ce96af8dd8bdb2a56f4a902b51a644bbdc3
Solej77 <ing.josgalo@gmail.com>
2020-04-04 00:13:25 -0600

    Creating the Home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
+import { Home } from './pages/Home'
 
 export const App = () => {
   const urlParams =  new window.URLSearchParams(window.location.search)
@@ -17,10 +17,10 @@ export const App = () => {
       {
         detailId
           ? <PhotoCardWithQuery id={detailId}/>
-          : <>
-              <ListOfCategories />
-              <ListOfPhotoCards categoryId={2}/>
-            </>
+          : <Router>
+              <Home path='/' />
+              <Home path='/pet/:id' />
+            </Router>
       }
     </>
   )
