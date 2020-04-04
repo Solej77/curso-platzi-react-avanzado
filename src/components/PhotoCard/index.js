57434fe9e43b6ac1c1524ca8fd76e94c98ab13c9
Solej77 <ing.josgalo@gmail.com>
2020-04-03 23:29:10 -0600

    Using the mutations with the likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import React from 'react'
+import { Article, ImgWrapper, Img } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,7 +11,6 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return(
     <Article ref={element}>
@@ -22,9 +22,25 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px'/> {likes} likes!
-          </Button>
+          {/* render prop, tiene que ser una funcion que indique que es lo que se quiere renderizar */}
+          <ToggleLikeMutation>
+            {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({ variables: {
+                    input: { id }
+                  }})
+                  setLiked(!liked)
+                }
+
+                return <FavButton 
+                  liked={liked}
+                  likes={likes}
+                  onClick={handleFavClick}
+                />
+              } 
+            }
+          </ToggleLikeMutation>
         </>
       }
     </Article>
