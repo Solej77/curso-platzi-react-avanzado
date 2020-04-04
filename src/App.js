55ed92d4612e0cde1f7a1cae4637250588e35392
Solej77 <ing.josgalo@gmail.com>
2020-04-04 15:42:03 -0600

    React:Context

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import Context from './Context'
 
-const UserLogged = ({ children }) => {
-return children({ isAuth: false })
-}
 
 export const App = () => {
   return(
@@ -24,7 +22,7 @@ export const App = () => {
         <Detail path='/detail/:detailId' />
       </Router>
 
-      <UserLogged>
+      <Context.Consumer>
         {
           ({ isAuth }) =>
             isAuth
@@ -37,7 +35,7 @@ export const App = () => {
                 <NotRegisteredUser path='/user' />
               </Router>
         }
-      </UserLogged>
+      </Context.Consumer>
       
       <Navbar />
     </>
