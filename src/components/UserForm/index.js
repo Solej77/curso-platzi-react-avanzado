533d943debcac8541ade5c631301fd2df0e6ec8a
Solej77 <ing.josgalo@gmail.com>
2020-04-05 01:18:11 -0600

    Sign off

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
-import { Title, Form, Input, Button, Error } from './styles'
+import { Title, Form, Input, Error } from './styles'
+import { SubmitButton } from '../SubmitButton'
 
 export const UserForm = ({ error, disabled, onSubmit, title }) => {
 
@@ -21,7 +22,7 @@ export const UserForm = ({ error, disabled, onSubmit, title }) => {
         <Title>{title}</Title>
         <Input disabled={disabled} placeholder='Email' {...email} />
         <Input disabled={disabled} placeholder='Password' type='password' {...password} />
-        <Button disabled={disabled}>{title}</Button>
+        <SubmitButton disabled={disabled}>{title}</SubmitButton>
       </Form>
       {error && <Error>{error}</Error>}
     </>
--- src/container/GetFavorites.js
+++ src/container/GetFavorites.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
-import { ListOfFavs } from '../components/ListOfFavs/index'
+import { ListOfFavs } from '../components/ListOfFavs'
 
 const GET_FAVS = gql`
   query getFavs {
@@ -15,14 +15,13 @@ const GET_FAVS = gql`
   }
 `
 
-const renderProp = ({ loading, error , data }) => {
-  if(loading) return <p>Loading...</p>
-  if(error) return <p>Error!</p>
+const renderProp = ({ loading, error, data }) => {
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
   console.error(error)
 
-  const { favs } = data
-  console.log(data)
-  return <ListOfFavs {...favs} />
+  const { favs = [] } = data
+  return <ListOfFavs favs={favs} />
 }
 
 export const FavsWithQuery = () => (
