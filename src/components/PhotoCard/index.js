35eb43ffe18d31eedb965a1e1228df9c5826670a
Solej77 <ing.josgalo@gmail.com>
2020-04-04 23:58:37 -0600

    Like as a registered user

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { Link } from '@reach/router'
 import { Article, ImgWrapper, Img } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes=0, src=DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+  
 
   return(
     <Article ref={element}>
@@ -28,10 +26,9 @@ export const PhotoCard = ({ id, likes=0, src=DEFAULT_IMAGE }) => {
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  !liked && toggleLike({ variables: {
+                  toggleLike({ variables: {
                     input: { id }
                   }})
-                  setLiked(!liked)
                 }
 
                 return <FavButton 
